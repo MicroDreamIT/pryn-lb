724a836382f0659acaee86fb794b3746c1b6ff6e
razib <rajib@microdreamit.com>
2019-02-06 11:36:42 +0600

    first_commit

---
+++ hospitalManagement/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('patient/', include('patient.urls')),
+    path('lab/', include('lab.urls')),
+    path('admin/', admin.site.urls),
+]
---
+++ patient/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index():
+    return HttpResponse("<h1>I am patient </h1>")
