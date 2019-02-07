c9891b0834be7925ad4ab67eaeebca71480e8b55
shahanursharif <sharif@microdreamit.com>
2019-02-07 17:26:40 +0600

    fixed font

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,13 +30,21 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * a simple convenience so we don't have to attach every token manually.
  */
 
-let token = document.head.querySelector('meta[name="csrf-token"]');
 
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+
+// let token = document.head.querySelector('meta[name="viewport"]');
+//
+// console.log(token)
+//
+// if (token) {
+//     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+// } else {
+//     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+// }
+axios.defaults.xsrfHeaderName = "X-CSRFToken"
+axios.defaults.xsrfCookieName = 'csrftoken'
+
+console.log(axios.defaults.xsrfHeaderName)
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
