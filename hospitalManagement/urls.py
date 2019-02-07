9ad02b57a7013e3c29c60691ad043e4065a0330c
shahanursharif <sharif@microdreamit.com>
2019-02-07 18:31:41 +0600

    fixed font

--- hospitalManagement/urls.py
+++ hospitalManagement/urls.py
@@ -1,10 +1,18 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
-    path('patient/', include('patient.urls')),
-    path('lab/', include('lab.urls')),
     path('admin/', admin.site.urls),
-
+    url(r'^.*$', views.index)
 ]
+
+
+# urlpatterns = [
+#     path('patient/', include('patient.urls')),
+#     path('lab/', include('lab.urls')),
+#     path('admin/', admin.site.urls),
+#
+# ]
--- resources/js/views/patient/create.vue
+++ resources/js/views/patient/create.vue
@@ -1,15 +1,47 @@
 <template>
     <div>
-        <h1>I am patient Create Page</h1>
+        <h1>Create Patient</h1>
+        <v-form v-model="valid">
+            <v-container>
+                <v-layout>
+                    <v-flex>
+                         <v-text-field
+                            v-model="name"
+                            :rules="nameRules"
+                            :counter="25"
+                            label="name"
+                            required
+                        ></v-text-field>
+                    </v-flex>
+                    <v-flex>
+                         <v-text-field
+                            v-model="age"
+                            :rules="ageRules"
+                            :counter="3"
+                            label="age"
+                            required
+                        ></v-text-field>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-form>
     </div>
 </template>
 
 <script>
     export default {
-        data(){
-            return{
-
-            }
-        }
+        data:()=>({
+            valid:false,
+            name:'',
+            age:0,
+            ageRules:[
+                v=>!!v||'Age required',
+                v=>v>0 && v.length<4 ||'age must be greater than 0 and less then 999',
+            ],
+            nameRules:[
+                v=>!!v||'Name is required',
+                v=>v.length<=25||'name must be less thn 10 characters'
+            ]
+        })
     }
 </script>
