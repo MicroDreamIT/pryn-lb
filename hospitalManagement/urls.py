e577422e2b15cbae20e71d64021335c84f52d12c
shahanursharif <sharif@microdreamit.com>
2019-02-07 16:03:25 +0600

    fixed font

--- hospitalManagement/urls.py
+++ hospitalManagement/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('patient/', include('patient.urls')),
     path('lab/', include('lab.urls')),
     path('admin/', admin.site.urls),
+
 ]
