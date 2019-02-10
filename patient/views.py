dac28a9d72a96335c9fa7810d58fffa32cf8d552
razib <rajib@microdreamit.com>
2019-02-10 11:31:59 +0600

    Data insert

--- patient/views.py
+++ patient/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Patient
 import json
 
+
 # Create your views here.
 def index(request):
     return HttpResponse('hello world i am index')
@@ -12,13 +13,13 @@ def create(request):
 
     if request.method == 'POST':
         data = json.loads(request.body)
-        Patient.objects.create(name=data['name'],
+        patient = Patient.objects.create(name=data['name'],
                                age=data['age'],
                                gender=data['gender'],
                                ref_no='R001',
                                care_of=data['care_of'],
                                address=data['address']
                                )
-        newData = Patient.objects.get(id=1)
-        return HttpResponse(newData)
+
+        return HttpResponse(Patient.objects.filter(pk=patient.id).values())
 
