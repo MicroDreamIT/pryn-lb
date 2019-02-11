902a03c1c1d3b41a67f5960e766650ec77663bd7
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2019-02-11 11:11:45 +0600

    working on forms

--- resources/js/views/patient/create.vue
+++ resources/js/views/patient/create.vue
@@ -1,25 +1,25 @@
 <template>
 
-      <v-container grid-list-lg class="form-style">
-          <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-layout row wrap >
-                                <v-flex xs12 md12>
-                                    <v-toolbar card   color="primary">
-                                     <v-toolbar-title class="body-2 white--text">Create New Patient</v-toolbar-title>
-                                </v-toolbar>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card-text class="px-0">
-                                          <v-text-field
-                                            v-model="name"
-                                            :counter="25"
-                                            label="Name"
-                                            required
-                                            data-vv-name="name"
-                                            v-validate="'required|max:25'"
-                                            :error-messages="errors.collect('name')"
-                                            clearable
-                                    ></v-text-field>
+    <v-container grid-list-lg class="form-style">
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-layout row wrap>
+                <v-flex xs12 md12>
+                    <v-toolbar card color="primary">
+                        <v-toolbar-title class="body-2 white--text">Create New Patient</v-toolbar-title>
+                    </v-toolbar>
+                </v-flex>
+                <v-flex xs12 md4>
+                    <v-card-text class="px-0">
+                        <v-text-field
+                                v-model="name"
+                                :counter="25"
+                                label="Name"
+                                required
+                                data-vv-name="name"
+                                v-validate="'required|max:25'"
+                                :error-messages="errors.collect('name')"
+                                clearable
+                        ></v-text-field>
 
                         <v-text-field
                                 v-model="age"
@@ -33,20 +33,28 @@
                                 clearable
                         ></v-text-field>
 
+                    </v-card-text>
+                </v-flex>
+                <v-flex xs12 md4>
+                    <v-card-text class="px-0">
                         <v-select
                                 :items="['Male', 'Female', 'Other']"
                                 label="Gender"
                                 v-model="gender"
                         ></v-select>
 
-                                    <v-text-field
-                                            v-model="care_of"
-                                            :counter="25"
-                                            label="Care of"
-                                            clearable
-                                    ></v-text-field>
-                                    </v-card-text>
+                        <v-text-field
+                                v-model="care_of"
+                                :counter="25"
+                                label="Care of"
+                                clearable
+                        ></v-text-field>
+                    </v-card-text>
 
+                </v-flex>
+                <v-flex xs12 md4>
+
+                    <v-card-text class="px-0">
                         <v-text-field
                                 v-model="mobile"
                                 mask="####-############"
@@ -54,25 +62,34 @@
                                 :counter="25"
                                 clearable
                         ></v-text-field>
-                         <v-textarea
-                            name="address"
-                            v-model="address"
-                            label="Address"
-                            hint="Address goes here"
-                            clearable
-                          ></v-textarea>
-                                </v-flex>
+                        <v-textarea
+                                name="address"
+                                v-model="address"
+                                label="Address"
+                                hint="Address goes here"
+                                clearable
+                        ></v-textarea>
+
+                    </v-card-text>
+
+                </v-flex>
+                <v-flex xs12 md4>
                     <v-card-actions>
-                        <v-btn @click="submit()" color="primary">submit</v-btn>
-                        <v-btn @click="clear()" color="error">clear</v-btn>
+                        <v-btn @click="submit()" color="primary">submit
+                            <v-icon dark right>check_circle</v-icon>
+                        </v-btn>
+
+                        <v-btn color="red" @click="clear()" dark>Decline
+                            <v-icon dark right>block</v-icon>
+                        </v-btn>
                     </v-card-actions>
+                </v-flex>
+            </v-layout>
+        </v-form>
 
-                  </v-layout>
+    </v-container>
 
-          </v-form>
-      </v-container>
 </template>
-
 
 
 <script src="./js/create.js"></script>
