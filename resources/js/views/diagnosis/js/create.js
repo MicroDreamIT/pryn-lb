68a1e65ef9fb841be35d95eb5e19fa2395320a19
razib <rajib@microdreamit.com>
2019-02-12 12:08:34 +0600

    js

---
+++ resources/js/views/diagnosis/js/create.js
@@ -0,0 +1,49 @@
+export default {
+    data:()=> ({
+        valid:true,
+        patient:{},
+        result:'',
+        type:'',
+        test:'',
+        isHidden:'',
+        checked_by:'',
+        technician:'',
+        date: new Date().toISOString().substr(0, 10),
+        menu: false,
+        modal: false,
+        appendCount:1,
+    }),
+    methods: {
+        submit() {
+            this.$validator.validateAll().then(value => {
+                if (value) {
+                    axios.post('/ajax/diagnosis/create', {
+                        name: this.name,
+                        result: this.result,
+                        opinion:this.opinion,
+                        advice:this.advice,
+                        prepared: this.prepared,
+                        checked_by: this.checked_by,
+                        technician: this.technician,
+                    }).then(res => {
+                        this.$root.successMessage(res.data)
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                }
+            })
+        },
+        addMore(){
+                this.appendCount++
+        },
+        clear() {
+            this.name = ''
+            this.result=''
+            this.opinion =''
+            this.advice = ''
+            this.prepared =''
+            this.checked_by=''
+            this.$validator.reset()
+        },
+    }
+}
